00ccd049c7451fcfe3e71881595ccde37ac922ee
yangg1488 <homerty9987loh@gmail.com>
2025-03-22 11:49:59 +0300

    add list

--- src/main/java/ru/example/edu/controller/StudentController.java
+++ src/main/java/ru/example/edu/controller/StudentController.java
@@ -1,4 +1,29 @@
 package ru.example.edu.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.example.edu.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class StudentController {
+    private long count = 1;
+    private List<Student> studentList = new ArrayList<>();
+
+    @GetMapping("/student")
+    public List<Student> getStudents() {
+        return studentList;
+    }
+
+    @PostMapping("/student")
+    public long addStudent(@RequestParam String name, @RequestParam String email, @RequestParam int age){
+        Student newStudent = new Student(count++, name, email, age);
+        studentList.add(newStudent);
+        return count;
+    }
+
 }
